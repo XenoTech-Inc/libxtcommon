ee899b45b5d4e76389d700dc35e75f05d3668358
Folkert van Verseveld <methos@xenotech.nl>
2016-11-18 11:50:11 +0100

    Collection demo, demos configure script fix, stack implemented
    The following types are implemented:
    short, int, unsigned, unsigned long, size_t
    
    Global configure script fixes including large file support

--- include/xt/collection.h
+++ include/xt/collection.h
@@ -117,6 +117,7 @@ bool xtHashmapPut(xtHashmap *map, void *key, void *value);
  * @return A boolean indicating if anything was found and is removed.
  */
 bool xtHashmapRemove(xtHashmap *map, void *key);
+
 /**
  * @brief A very easy to use (array) list.
  *
@@ -208,6 +209,142 @@ void xtListSetElementDestroyFunc(xtList *list, void (*destroyElementFunc) (xtLis
  * Specify a null pointer to remove the function.
  */
 void xtListSetListDestroyFunc(xtList *list, void (*destroyListFunc) (xtList *list));
+
+/* STACK */
+
+/*
+ * NOTE: all stack operations are only documented for xtStackD,
+ * since all other types (xtStackU, etc.) are exactly the same.
+ */
+
+#define XT_STACK_CAPACITY_DEFAULT 256
+
+struct xtStackHD {
+	short *data;
+	size_t count, capacity;
+	int grow;
+};
+
+struct xtStackD {
+	int *data;
+	size_t count, capacity;
+	int grow;
+};
+
+struct xtStackU {
+	unsigned *data;
+	size_t count, capacity;
+	int grow;
+};
+
+struct xtStackLU {
+	unsigned long *data;
+	size_t count, capacity;
+	int grow;
+};
+
+struct xtStackZU {
+	size_t *data;
+	size_t count, capacity;
+	int grow;
+};
+
+void xtStackHDInit(struct xtStackHD *stack);
+void xtStackDInit (struct xtStackD  *stack);
+void xtStackUInit (struct xtStackU  *stack);
+void xtStackLUInit(struct xtStackLU *stack);
+void xtStackZUInit(struct xtStackZU *stack);
+
+void xtStackHDDestroy(struct xtStackHD *stack);
+void xtStackDDestroy (struct xtStackD  *stack);
+void xtStackUDestroy (struct xtStackU  *stack);
+void xtStackLUDestroy(struct xtStackLU *stack);
+void xtStackZUDestroy(struct xtStackZU *stack);
+/**
+ * Creates a new stack. By default, automatic growth is enabled.
+ * @param capacity Initial capacity (specify zero to use default value).
+ * @return Zero if the stack has been created successfully, otherwise an error code.
+ */
+int xtStackDCreate (struct xtStackD  *stack, size_t capacity);
+int xtStackHDCreate(struct xtStackHD *stack, size_t capacity);
+int xtStackUCreate (struct xtStackU  *stack, size_t capacity);
+int xtStackLUCreate(struct xtStackLU *stack, size_t capacity);
+int xtStackZUCreate(struct xtStackZU *stack, size_t capacity);
+/**
+ * Changes grow policy. Positive values indicate fixed growth.
+ * Negative values is relative growth. Zero disables growth.
+ * E.g. grow(-2) is 100% / 2 = 50%, grow(-5) is 100% / 5 = 20%
+ */
+void xtStackDSetGrow (struct xtStackD  *stack, int grow);
+void xtStackHDSetGrow(struct xtStackHD *stack, int grow);
+void xtStackUSetGrow (struct xtStackU  *stack, int grow);
+void xtStackLUSetGrow(struct xtStackLU *stack, int grow);
+void xtStackZUSetGrow(struct xtStackZU *stack, int grow);
+/**
+ * Get grow policy. Positive values indicate fixed growth.
+ * Negative values is relative growth. Zero disables growth.
+ * E.g. grow(-2) is 100% / 2 = 50%, grow(-5) is 100% / 5 = 20%
+ */
+int xtStackDGetGrow (struct xtStackD  *stack);
+int xtStackHDGetGrow(struct xtStackHD *stack);
+int xtStackUGetGrow (struct xtStackU  *stack);
+int xtStackLUGetGrow(struct xtStackLU *stack);
+int xtStackZUGetGrow(struct xtStackZU *stack);
+/**
+ * Attempts to add \a value to stack.
+ * The list will grow if configured to do so.
+ * @return Zero if \a value has been added, otherwise an error code.
+ */
+int xtStackDPush (struct xtStackD  *stack, int value);
+int xtStackHDPush(struct xtStackHD *stack, short value);
+int xtStackUPush (struct xtStackU  *stack, unsigned value);
+int xtStackLUPush(struct xtStackLU *stack, unsigned long value);
+int xtStackZUPush(struct xtStackZU *stack, size_t value);
+/**
+ * Fetches last pushed item. Fails if stack is empty.
+ */
+bool xtStackDPeek (struct xtStackD  *stack, int *top);
+bool xtStackHDPeek(struct xtStackHD *stack, short *top);
+bool xtStackUPeek (struct xtStackU  *stack, unsigned *top);
+bool xtStackLUPeek(struct xtStackLU *stack, unsigned long *top);
+bool xtStackZUPeek(struct xtStackZU *stack, size_t *top);
+/**
+ * Free elements or do nothing if NULL has been specified.
+ */
+void xtStackDDestroy (struct xtStackD  *stack);
+void xtStackHDDestroy(struct xtStackHD *stack);
+void xtStackUDestroy (struct xtStackU  *stack);
+void xtStackLUDestroy(struct xtStackLU *stack);
+void xtStackZUDestroy(struct xtStackZU *stack);
+/**
+ * Stores and removes last pushed item. Fails if stack is empty.
+ */
+bool xtStackDPop (struct xtStackD  *stack, int *top);
+bool xtStackHDPop(struct xtStackHD *stack, short *top);
+bool xtStackUPop (struct xtStackU  *stack, unsigned *top);
+bool xtStackLUPop(struct xtStackLU *stack, unsigned long *top);
+bool xtStackZUPop(struct xtStackZU *stack, size_t *top);
+/**
+ * Removes last pushed item and return new count.
+ * Fails if stack is empty (i.e. returns zero).
+ */
+size_t xtStackDDrop (struct xtStackD  *stack);
+size_t xtStackHDDrop(struct xtStackHD *stack);
+size_t xtStackUDrop (struct xtStackU  *stack);
+size_t xtStackLUDrop(struct xtStackLU *stack);
+size_t xtStackZUDrop(struct xtStackZU *stack);
+
+size_t xtStackHDGetSize(struct xtStackHD *stack);
+size_t xtStackDGetSize (struct xtStackD *stack);
+size_t xtStackUGetSize (struct xtStackU *stack);
+size_t xtStackLUGetSize(struct xtStackLU *stack);
+size_t xtStackZUGetSize(struct xtStackZU *stack);
+
+size_t xtStackHDGetCapacity(struct xtStackHD *stack);
+size_t xtStackDGetCapacity (struct xtStackD *stack);
+size_t xtStackUGetCapacity (struct xtStackU *stack);
+size_t xtStackLUGetCapacity(struct xtStackLU *stack);
+size_t xtStackZUGetCapacity(struct xtStackZU *stack);
 
 #ifdef __cplusplus
 }
--- src/generic/collection.c
+++ src/generic/collection.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define XT_LIST_CAPACITY_DEFAULT 1024
+
 static void xtHashmapDeleteBucket(struct xtHashBucket *bucket)
 {
 	free(bucket->value);
@@ -234,7 +236,7 @@ void xtListClear(xtList *list)
 int xtListCreate(xtList *list, size_t capacity)
 {
 	if (capacity == 0)
-		capacity = 1024;
+		capacity = XT_LIST_CAPACITY_DEFAULT;
 	void **data = malloc(capacity * sizeof(void*));
 	if (!data)
 		return XT_ENOMEM;
@@ -333,3 +335,297 @@ void xtListSetListDestroyFunc(xtList *list, void (*destroyListFunc) (xtList *lis
 {
 	list->destroyListFunc = destroyListFunc;
 }
+
+/* STACK */
+
+#define func_init(type) void type ## Init(struct type *t) { t->data = NULL; }
+#define func_set_grow(type) void type ## SetGrow(struct type *t, int grow) { t->grow = grow; }
+#define func_get_grow(type) int type ## GetGrow(struct type *t) { return t->grow; }
+
+func_init(xtStackHD)
+func_init(xtStackD)
+func_init(xtStackU)
+func_init(xtStackLU)
+func_init(xtStackZU)
+
+func_set_grow(xtStackHD)
+func_set_grow(xtStackD)
+func_set_grow(xtStackU)
+func_set_grow(xtStackLU)
+func_set_grow(xtStackZU)
+
+func_get_grow(xtStackHD)
+func_get_grow(xtStackD)
+func_get_grow(xtStackU)
+func_get_grow(xtStackLU)
+func_get_grow(xtStackZU)
+
+static inline int xt_stack_create(void **data, size_t elemsize, size_t n)
+{
+	if (!n)
+		n = XT_STACK_CAPACITY_DEFAULT;
+	if (!(*data = malloc(n * elemsize)))
+		return XT_ENOMEM;
+	return 0;
+}
+
+#define stack_init(this, data, cap) \
+	this->data = data;\
+	this->count = 0;\
+	this->capacity = cap;\
+	this->grow = -2;
+
+int xtStackHDCreate(struct xtStackHD *this, size_t capacity)
+{
+	void *data;
+	int ret = xt_stack_create(&data, sizeof(short), capacity);
+	if (ret)
+		return ret;
+	stack_init(this, data, capacity);
+	return 0;
+}
+
+int xtStackDCreate(struct xtStackD *this, size_t capacity)
+{
+	void *data;
+	int ret = xt_stack_create(&data, sizeof(int), capacity);
+	if (ret)
+		return ret;
+	stack_init(this, data, capacity)
+	return 0;
+}
+
+int xtStackUCreate(struct xtStackU *this, size_t capacity)
+{
+	void *data;
+	int ret = xt_stack_create(&data, sizeof(unsigned), capacity);
+	if (ret)
+		return ret;
+	stack_init(this, data, capacity)
+	return 0;
+}
+
+int xtStackLUCreate(struct xtStackLU *this, size_t capacity)
+{
+	void *data;
+	int ret = xt_stack_create(&data, sizeof(unsigned long), capacity);
+	if (ret)
+		return ret;
+	stack_init(this, data, capacity)
+	return 0;
+}
+
+int xtStackZUCreate(struct xtStackZU *this, size_t capacity)
+{
+	void *data;
+	int ret = xt_stack_create(&data, sizeof(size_t), capacity);
+	if (ret)
+		return ret;
+	stack_init(this, data, capacity)
+	return 0;
+}
+
+#define func_free(type) void type ## Destroy(struct type *t) { if (t->data) { free(t->data); t->data = NULL; } }
+
+func_free(xtStackD )
+func_free(xtStackHD)
+func_free(xtStackU )
+func_free(xtStackLU)
+func_free(xtStackZU)
+
+int xtStackHDPush(struct xtStackHD *this, short value)
+{
+	if (this->count != this->capacity)
+		goto push;
+	if (!this->grow)
+		return XT_ENOBUFS;
+	size_t grow = this->grow > 0 ? (unsigned)this->grow : this->capacity / -(unsigned)this->grow;
+	// grow at least by one
+	if (!grow) ++grow;
+	short *tmp = realloc(this->data, this->capacity + grow);
+	if (!tmp)
+		return XT_ENOMEM;
+	this->data = tmp;
+	this->capacity += grow;
+push:
+	this->data[this->count++] = value;
+	return 0;
+}
+
+int xtStackDPush(struct xtStackD *this, int value)
+{
+	if (this->count != this->capacity)
+		goto push;
+	if (!this->grow)
+		return XT_ENOBUFS;
+	size_t grow = this->grow > 0 ? (unsigned)this->grow : this->capacity / -(unsigned)this->grow;
+	// grow at least by one
+	if (!grow) ++grow;
+	int *tmp = realloc(this->data, this->capacity + grow);
+	if (!tmp)
+		return XT_ENOMEM;
+	this->data = tmp;
+	this->capacity += grow;
+push:
+	this->data[this->count++] = value;
+	return 0;
+}
+
+int xtStackUPush(struct xtStackU *this, unsigned value)
+{
+	if (this->count != this->capacity)
+		goto push;
+	if (!this->grow)
+		return XT_ENOBUFS;
+	size_t grow = this->grow > 0 ? (unsigned)this->grow : this->capacity / -(unsigned)this->grow;
+	// grow at least by one
+	if (!grow) ++grow;
+	unsigned *tmp = realloc(this->data, this->capacity + grow);
+	if (!tmp)
+		return XT_ENOMEM;
+	this->data = tmp;
+	this->capacity += grow;
+push:
+	this->data[this->count++] = value;
+	return 0;
+}
+
+int xtStackLUPush(struct xtStackLU *this, unsigned long value)
+{
+	if (this->count != this->capacity)
+		goto push;
+	if (!this->grow)
+		return XT_ENOBUFS;
+	size_t grow = this->grow > 0 ? (unsigned)this->grow : this->capacity / -(unsigned)this->grow;
+	// grow at least by one
+	if (!grow) ++grow;
+	unsigned long *tmp = realloc(this->data, this->capacity + grow);
+	if (!tmp)
+		return XT_ENOMEM;
+	this->data = tmp;
+	this->capacity += grow;
+push:
+	this->data[this->count++] = value;
+	return 0;
+}
+
+int xtStackZUPush(struct xtStackZU *this, size_t value)
+{
+	if (this->count != this->capacity)
+		goto push;
+	if (!this->grow)
+		return XT_ENOBUFS;
+	size_t grow = this->grow > 0 ? (unsigned)this->grow : this->capacity / -(unsigned)this->grow;
+	// grow at least by one
+	if (!grow) ++grow;
+	size_t *tmp = realloc(this->data, this->capacity + grow);
+	if (!tmp)
+		return XT_ENOMEM;
+	this->data = tmp;
+	this->capacity += grow;
+push:
+	this->data[this->count++] = value;
+	return 0;
+}
+
+bool xtStackHDPeek(struct xtStackHD *this, short *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[this->count - 1];
+	return true;
+}
+
+bool xtStackDPeek(struct xtStackD *this, int *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[this->count - 1];
+	return true;
+}
+
+bool xtStackUPeek(struct xtStackU *this, unsigned *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[this->count - 1];
+	return true;
+}
+
+bool xtStackLUPeek(struct xtStackLU *this, unsigned long *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[this->count - 1];
+	return true;
+}
+
+bool xtStackZUPeek(struct xtStackZU *this, size_t *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[this->count - 1];
+	return true;
+}
+
+bool xtStackHDPop(struct xtStackHD *this, short *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[--this->count];
+	return true;
+}
+
+bool xtStackDPop(struct xtStackD *this, int *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[--this->count];
+	return true;
+}
+
+bool xtStackUPop(struct xtStackU *this, unsigned *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[--this->count];
+	return true;
+}
+
+bool xtStackLUPop(struct xtStackLU *this, unsigned long *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[--this->count];
+	return true;
+}
+
+bool xtStackZUPop(struct xtStackZU *this, size_t *top)
+{
+	if (!this->count)
+		return false;
+	*top = this->data[--this->count];
+	return true;
+}
+
+#define func_drop(type) size_t type ## Drop(struct type *t) { return t->count ? --t->count : 0; }
+#define func_get_size(type) size_t type ## GetSize(struct type *t) { return t->count; }
+#define func_get_cap(type) size_t type ## GetCapacity(struct type *t) { return t->capacity; }
+
+func_drop(xtStackHD)
+func_drop(xtStackD )
+func_drop(xtStackU )
+func_drop(xtStackLU)
+func_drop(xtStackZU)
+
+func_get_size(xtStackHD)
+func_get_size(xtStackD)
+func_get_size(xtStackU )
+func_get_size(xtStackLU)
+func_get_size(xtStackZU)
+
+func_get_cap(xtStackHD)
+func_get_cap(xtStackD )
+func_get_cap(xtStackU )
+func_get_cap(xtStackLU)
+func_get_cap(xtStackZU)
